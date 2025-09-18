028fc7ab7c40b00f9ba704ea727556fce6660881
Minsoo Kim <vv137.xyz@gmail.com>
2025-09-18 13:09:40 +0900

    feat: load_sabmark take name

--- otalign/data/hf_loading.py
+++ otalign/data/hf_loading.py
@@ -1,13 +1,13 @@
 from datasets import load_dataset
 
 
-def load_malidup(split: str = "test"):
-    return load_dataset("DeepFoldProtein/malidup-dataset", split=split)
+def load_malidup():
+    return load_dataset("DeepFoldProtein/malidup-dataset", split="test")
 
 
-def load_malisam(split: str = "test"):
-    return load_dataset("DeepFoldProtein/malisam-dataset", split=split)
+def load_malisam():
+    return load_dataset("DeepFoldProtein/malisam-dataset", split="test")
 
 
-def load_sabmark(split: str = "test"):
-    return load_dataset("DeepFoldProtein/SABmark-dataset", split=split)
+def load_sabmark(name: str = "all"):
+    return load_dataset("DeepFoldProtein/SABmark-dataset", name=name, split="test")
